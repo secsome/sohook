9e46b3b45fef4ea6a2bea6e8cea163241b58bf5f
secsome <302702960@qq.com>
2024-04-15 22:10:17 +0800

    Update sohook header

--- sohook.h
+++ sohook.h
@@ -1,11 +1,55 @@
 #pragma once
 
 #include <stddef.h>
+#include <stdint.h>
 #include <sys/user.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+union register_item
+{
+    uint64_t qword;
+    int64_t sqword;
+    uint32_t dwords[2];
+    int32_t sdwords[2];
+    uint16_t words[4];
+    int16_t swords[4];
+    uint8_t bytes[8];
+    int8_t sbytes[8];
+};
+
+struct REGISTERS
+{
+    union register_item r15;
+    union register_item r14;
+    union register_item r13;
+    union register_item r12;
+    union register_item rbp;
+    union register_item rbx;
+    union register_item r11;
+    union register_item r10;
+    union register_item r9;
+    union register_item r8;
+    union register_item rax;
+    union register_item rcx;
+    union register_item rdx;
+    union register_item rsi;
+    union register_item rdi;
+    union register_item orig_rax;
+    union register_item rip;
+    union register_item cs;
+    union register_item eflags;
+    union register_item rsp;
+    union register_item ss;
+    union register_item fs_base;
+    union register_item gs_base;
+    union register_item ds;
+    union register_item es;
+    union register_item fs;
+    union register_item gs;
+};
 
 struct hookdecl_t
 {
@@ -18,9 +62,9 @@ struct hookdecl_t
 #define __STR(x) #x
 
 #define DEFINE_HOOK(addr, name, size) \
-size_t _func_ ## name ## _(struct user_regs_struct* R); \
+size_t _func_ ## name ## _(struct REGISTERS* R); \
 __attribute__((section(".sohook"))) struct hookdecl_t _ ## name ## _hookdecls_ = { (void*)addr, size, __STR(_func_ ## name ## _) }; \
-size_t _func_ ## name ## _(struct user_regs_struct* R)
+size_t _func_ ## name ## _(struct REGISTERS* R)
 
 struct funcdecl_t
 {
--- test.c
+++ test.c
@@ -7,6 +7,6 @@ DEFINE_FUNC(0x1189, add_ptr, int, int, int);
 
 DEFINE_HOOK(0x11F9, func_add, 0x5)
 {
-    R->rdi = add_ptr(R->rdi & 0xffffffff, R->rsi & 0xffffffff);
+    R->rdi.dwords[0] = add_ptr(R->rdi.dwords[0], R->rsi.dwords[0]);
     return 0;
 }
